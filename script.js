3ec5b7f97a133a7c56c74ca8edce1bf5382d8834
cruzjeff225 <cruzjeff225@gmail.com>
2023-06-10 12:11:52 -0600

    En este commit ya comienzo a trabajar el js

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Seleccionamos los datos a usar*/
+const pokemonCard = document.querySelector('[data-pokemon-card]');
+const pokemonName = document.querySelector('[data-pokemon-name]');
+const pokemonImg = document.querySelector('[data-pokemon-img]');
+const pokemonImgContainer = document.querySelector('[data-pokemon-img-container]');
+const pokemonId = document.querySelector('[data-pokemon-id]');
+const pokemonTypes= document.querySelector('[data-pokemon-types]');
+const pokemonStats= document.querySelector('[data-pokemon-stats]');
+
